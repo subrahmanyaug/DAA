b1147e80f9d088d3317533ec86070db0ceb3177c
Subrahmanya UG <subrahmanyaug@gmail.com>
2023-07-30 21:46:19 +0530

    Bubble sort

---
+++ Bubble_Sort.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Bubble_Sort{
+    public static void main(String[] args) {
+
+       System.out.println("enter array size:");
+       Scanner S=new Scanner(System.in);
+       int size=S.nextInt();
+       int[] array=new int[size];
+       System.out.println("enter array elements:");
+       for(int i=0;i<size;i++)
+       {
+           array[i]=S.nextInt();
+       }
+        for (int i = 0; i <=size - 2; i++) {
+
+            for (int j = 0; j <= size-2-i; j++) {
+                if (array[j+1] < array[j]) {
+                    // Swap array[i] and array[min]
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+
+
+        }
+
+        // Print the sorted array
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+        }
+    }
